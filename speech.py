153f29044f46ff587890d58dd10bbbab9c144a07
brandoshizzle <cathcart.brandon@gmail.com>
2020-05-12 14:48:49 -0600

    adding vocab

--- speech.py
+++ speech.py
@@ -9,20 +9,28 @@ starters_good_list = [
     "Sick bro! ",
     "SKKKRAT SKKKRAT! ",
     "Hold my spitter, bro... ",
+    "Like, dude! ",
+    "Wheel, snipe, celly boys. "
 ]
 
 starters_neutral_list = ["Suh dude, ", "Bro, ",
-                         "Bud, ", "HEY GUYS, Dicebro here. ", "Three things are gunna happen: I hit you, you hit the ground, and "]
+                         "Bud, ", "HEY GUYS, Dicebro here. ", "Three things are gunna happen: I hit you, you hit the ground, and ", "skriii brappp bwooraappp! "]
 
 starters_bad_list = ["Shit bro, ",
-                     "Sheeeeeeeeeeeeeet. " "Damn bro, ", "My guy... ", "Give your balls a tug. "]
+                     "Sheeeeeeeeeeeeeet. " "Damn bro, ", "My guy... ", "Give your balls a tug. ", "Jinkies!", "Jeepers!", "Zoinks!", "Ruh-Roh!"]
 
 you_rolled_list = ["rolled a", "laid down a",
                    "tossed a", "flipped a", "flipped a"]
 
-crit_fail = ["Everybody's dying :(", "NO DON'T", "f in the chat"]
+crit_fail = ["Everybody's dying :(", "NO DON'T",
+             "f in the chat", "Ya fucked er, bud."]
 
-crit_success = ["YOOOOOOOOOOOOOOO", "BROOOOO BRO. YEH. BRO."]
+crit_success = ["YOOOOOOOOOOOOOOO",
+                "BROOOOO BRO. YEH. BRO.", "COME ON AND SLAM!"]
+
+just_chatting = ["Gonna crush some sandos after prackie."]
+
+sfx = [[""]]
 
 
 def starters(kind: str):
